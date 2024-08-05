efe9e4b682a7767c29c7de03ce0b9a1b8e539314
Paul <paulb@dds.nl>
2024-08-05 23:05:31 +0200

    Add decoder for Victron SmartShunt data on port 11

--- src/TTN/plain_decoder.js
+++ src/TTN/plain_decoder.js
@@ -1,88 +1,119 @@
 // Decoder for device payload encoder "PLAIN"
-// copy&paste to TTN Console -> Applications -> PayloadFormat -> Decoder
+// copy&paste to TTN Console V3 -> Applications -> Payload formatters -> Uplink -> Javascript
+// modified for The Things Stack V3 by Caspar Armster, dasdigidings e.V.
 
-function Decoder(bytes, port) {
-  var decoded = {};
+function decodeUplink(input) {
+  var data = {};
 
-  if (port === 1) {
-    var i = 0;
-
-    if (bytes.length >= 2) {
-      decoded.wifi = (bytes[i++] << 8) | bytes[i++];
-    }
-     
-    if (bytes.length === 4 || bytes.length > 15) {
-      decoded.ble = (bytes[i++] << 8) | bytes[i++];
-    }
-
-    if (bytes.length > 4) {
-      decoded.latitude = ((bytes[i++] << 24) | (bytes[i++] << 16) | (bytes[i++] << 8) | bytes[i++]);
-      decoded.longitude = ((bytes[i++] << 24) | (bytes[i++] << 16) | (bytes[i++] << 8) | bytes[i++]);
-      decoded.sats = bytes[i++];
-      decoded.hdop = (bytes[i++] << 8) | (bytes[i++]);
-      decoded.altitude = ((bytes[i++] << 8) | (bytes[i++]));
-    }
-  }
-
-  if (port === 2) {
-    var i = 0;
-    decoded.battery = ((bytes[i++] << 8) | bytes[i++]);
-    decoded.uptime = ((bytes[i++] << 56) | (bytes[i++] << 48) | (bytes[i++] << 40) | (bytes[i++] << 32) |
-      (bytes[i++] << 24) | (bytes[i++] << 16) | (bytes[i++] << 8) | bytes[i++]);
-    decoded.temp = bytes[i++];
-    decoded.memory = ((bytes[i++] << 24) | (bytes[i++] << 16) | (bytes[i++] << 8) | bytes[i++]);
-    decoded.reset0 = bytes[i++];
-    decoded.restarts = ((bytes[i++] << 24) | (bytes[i++] << 16) | (bytes[i++] << 8) | bytes[i++]);
-  }
-
-  if (port === 4) {
-    var i = 0;
-    decoded.latitude = ((bytes[i++] << 24) | (bytes[i++] << 16) | (bytes[i++] << 8) | bytes[i++]);
-    decoded.longitude = ((bytes[i++] << 24) | (bytes[i++] << 16) | (bytes[i++] << 8) | bytes[i++]);
-    decoded.sats = bytes[i++];
-    decoded.hdop = (bytes[i++] << 8) | (bytes[i++]);
-    decoded.altitude = ((bytes[i++] << 8) | (bytes[i++]));
-  }
-
-  if (port === 5) {
-    var i = 0;
-    decoded.button = bytes[i++];
-  }
-
-  if (port === 7) {
-    var i = 0;
-    decoded.temperature = ((bytes[i++] << 8) | bytes[i++]);
-    decoded.pressure = ((bytes[i++] << 8) | bytes[i++]);
-    decoded.humidity = ((bytes[i++] << 8) | bytes[i++]);
-    decoded.air = ((bytes[i++] << 8) | bytes[i++]);
-  }
-
-  if (port === 8) {
-    var i = 0;
-    if (bytes.length >= 2) {
-      decoded.battery = (bytes[i++] << 8) | bytes[i++];
-    }
-  }
-
-  if (port === 9) {
-    // timesync request
-    if (bytes.length === 1) {
-      decoded.timesync_seqno = bytes[0];
-    }
-    // epoch time answer
-    if (bytes.length === 5) {
+  if (input.fPort === 1) {
       var i = 0;
-      decoded.time = ((bytes[i++] << 24) | (bytes[i++] << 16) | (bytes[i++] << 8) | bytes[i++]);
-      decoded.timestatus = bytes[i++];
-    }
+
+      if (input.bytes.length >= 2) {
+          data.wifi = (input.bytes[i++] << 8) | input.bytes[i++];
+      }
+   
+      if (input.bytes.length === 4 || input.bytes.length > 15) {
+          data.ble = (input.bytes[i++] << 8) | input.bytes[i++];
+      }
+
+      if (input.bytes.length > 4) {
+          data.latitude = ((input.bytes[i++] << 24) | (input.bytes[i++] << 16) | (input.bytes[i++] << 8) | input.bytes[i++]);
+          data.longitude = ((input.bytes[i++] << 24) | (input.bytes[i++] << 16) | (input.bytes[i++] << 8) | input.bytes[i++]);
+          data.sats = input.bytes[i++];
+          data.hdop = (input.bytes[i++] << 8) | input.bytes[i++];
+          data.altitude = ((input.bytes[i++] << 8) | input.bytes[i++]);
+      }
+      data.pax = 0;
+      if ('wifi' in data) {
+          data.pax += data.wifi;
+      }
+      if ('ble' in data) {
+          data.pax += data.ble;
+      } 
   }
 
-  if (port === 10) {
-    var i = 0;
-    if (bytes.length >= 2) {
-      decoded.ens = (bytes[i++] << 8) | bytes[i++];
-    }
+  if (input.fPort === 2) {
+      var i = 0;
+      data.voltage = ((input.bytes[i++] << 8) | input.bytes[i++]);
+      data.uptime = ((input.bytes[i++] << 56) | (input.bytes[i++] << 48) | (input.bytes[i++] << 40) | (input.bytes[i++] << 32) | (input.bytes[i++] << 24) | (input.bytes[i++] << 16) | (input.bytes[i++] << 8) | input.bytes[i++]);
+      data.cputemp = input.bytes[i++];
+      data.memory = ((input.bytes[i++] << 24) | (input.bytes[i++] << 16) | (input.bytes[i++] << 8) | input.bytes[i++]);
+      data.reset0 = input.bytes[i++];
+      data.restarts = ((input.bytes[i++] << 24) | (input.bytes[i++] << 16) | (input.bytes[i++] << 8) | input.bytes[i++]);
   }
 
-  return decoded;
+  if (input.fPort === 4) {
+      var i = 0;
+      data.latitude = ((input.bytes[i++] << 24) | (input.bytes[i++] << 16) | (input.bytes[i++] << 8) | input.bytes[i++]);
+      data.longitude = ((input.bytes[i++] << 24) | (input.bytes[i++] << 16) | (input.bytes[i++] << 8) | input.bytes[i++]);
+      data.sats = input.bytes[i++];
+      data.hdop = (input.bytes[i++] << 8) | (input.bytes[i++]);
+      data.altitude = ((input.bytes[i++] << 8) | input.bytes[i++]);
+  }
+
+  if (input.fPort === 5) {
+      var i = 0;
+      data.button = input.bytes[i++];
+  }
+
+  if (input.fPort === 7) {
+      var i = 0;
+      data.temperature = ((input.bytes[i++] << 8) | input.bytes[i++]);
+      data.pressure = ((input.bytes[i++] << 8) | input.bytes[i++]);
+      data.humidity = ((input.bytes[i++] << 8) | input.bytes[i++]);
+      data.air = ((input.bytes[i++] << 8) | input.bytes[i++]);
+  }
+
+  if (input.fPort === 8) {
+      var i = 0;
+      if (input.bytes.length >= 2) {
+          data.voltage = (input.bytes[i++] << 8) | input.bytes[i++];
+      }
+  }
+
+  if (input.fPort === 9) {
+      // timesync request
+      if (input.bytes.length === 1) {
+          data.timesync_seqno = input.bytes[0];
+      }
+      // epoch time answer
+      if (input.bytes.length === 5) {
+          var i = 0;
+          data.time = ((input.bytes[i++] << 24) | (input.bytes[i++] << 16) | (input.bytes[i++] << 8) | input.bytes[i++]);
+          data.timestatus = input.bytes[i++];
+      }
+  }
+
+  if (input.fPort === 10) {
+      var i = 0;
+      if (input.bytes.length >= 2) {
+          data.ens = (input.bytes[i++] << 8) | input.bytes[i++];
+      }
+  }
+
+  if (input.fPort === 11) {
+      // Decoding Victron SmartShunt data
+      var i = 0;
+      data.battery_voltage = ((input.bytes[i++] << 8) | input.bytes[i++]);
+      data.current = ((input.bytes[i++] << 8) | input.bytes[i++]);
+      data.power = ((input.bytes[i++] << 8) | input.bytes[i++]);
+      data.state_of_charge = input.bytes[i++];
+      data.temperature = ((input.bytes[i++] << 8) | input.bytes[i++]);
+      data.time_to_go = ((input.bytes[i++] << 8) | input.bytes[i++]);
+  }
+
+  if (data.hdop) {
+      data.hdop /= 100;
+      data.latitude /= 1000000;
+      data.longitude /= 1000000;
+  }
+
+  data.bytes = input.bytes; // comment out if you do not want to include the original payload
+  data.port = input.fPort; // comment out if you do not want to include the port
+
+  return {
+      data: data,
+      warnings: [],
+      errors: []
+  };
 }
